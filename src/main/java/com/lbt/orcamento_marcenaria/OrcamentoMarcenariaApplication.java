65c9cf997ffa87aca53e50c894445b82334ab6ca
renanNobrega <renan.pereira@anjunexpress.com.br>
2024-01-16 17:21:04 -0300

    firstCommit

---
+++ src/main/java/com/lbt/orcamento_marcenaria/OrcamentoMarcenariaApplication.java
@@ -0,0 +1,13 @@
+package com.lbt.orcamento_marcenaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentoMarcenariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentoMarcenariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lbt/orcamento_marcenaria/OrcamentoMarcenariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbt.orcamento_marcenaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoMarcenariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
